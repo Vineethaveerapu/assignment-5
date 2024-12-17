bd80a0b7d62d7ac57e510d495cc5d6944935d4c9
vineetha <vineethaveerapu4@gmail.com>
2024-12-17 17:19:48 +0100

    added images

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
+const imagesCollection = [
+  "bouquet.png",
+  "cake.png",
+  "christmastree.png",
+  "dog.png",
+  "fish.png",
+  "fruits.png",
+  "house.png",
+  "present.png",
+  "smiley.png",
+  "snowman.png"
+];
+
 function createGameBoard() {
-  console.log("hello world");
+  const gameBoard = document.querySelector(".game-board");
+
+  // shuffle the imagesCollection array and create card images with six pairs (12 total)
+  const shuffledImages = imagesCollection.sort(() => Math.random() - 0.5);
+  const sixImages = shuffledImages.slice(0, 6);
+  const cardImages = [...sixImages, ...sixImages].sort(
+    () => Math.random() - 0.5
+  );
+
+  cardImages.forEach((card) => {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+
+    const img = document.createElement("img");
+    img.classList.add("card-image");
+    img.src = `./images/${card}`;
+    cardElement.appendChild(img);
+
+    gameBoard.appendChild(cardElement);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
