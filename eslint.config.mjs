e8bc6b5f17b6efa64b359f890aaae74a8f0f8a42
vineetha <vineethaveerapu4@gmail.com>
2025-01-04 14:12:26 +0100

    refactor

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: { globals: globals.browser },
+    ignores: ["scripts/external-dependencies/"]
+  },
+  pluginJs.configs.recommended
 ];
